ae938a66bbe7293eca37f8ede0aefa69a82a671c
gov <gabcomp.gov@gmail.com>
2021-01-25 14:39:24 -0300

    s002v001
    docs add Script 002

---
+++ 002_string_types.rb
@@ -0,0 +1,37 @@
+#002_String_Types
+
+#Listas para Saida de Texto
+string_list_001 = "\nPrimeiro Array",32,32,32;
+string_list_002 = "\nSegundo Array",101,101,101;
+string_list_003 = 32,32;
+string_list_004 = 101,101;
+
+puts string_list_001 , string_list_002
+
+#Pula Linha (\n\n)
+puts
+
+#Saida de Letra Unica
+puts "Letra Unica"
+puts ?H
+
+#Pula Linha (\n\n)
+puts
+
+#Saida de Ascii
+puts "Ascii da Letra"
+puts "H".ord
+
+name = "\n\nOther String\n\n";
+#Print interpretado
+puts name
+#Print n interpretado
+p name
+
+#Pula Linha (\n\n)
+puts
+
+#Saida de Lista dentro de String
+puts "Array 03: #{string_list_003} Array 04: #{string_list_003}"
+
+
